7169f67cd950a76f25878ef7221b4421f0549cf6
ilangovenac <acilangoven@gmail.com>
2020-12-18 10:25:29 +0530

    Control flow opertors

---
+++ controlflow.py
@@ -0,0 +1,81 @@
+
+#place=input('Enter your place: ')
+#if place == 'Chennai':
+#    print('Its the capital of Tamil Nadu')
+##Tab indentation is necessary
+
+#/////////////
+
+#place=input('Enter your place: ')
+#if place == 'Chennai':
+#    print('Its the capital of Tamil Nadu')
+#Tab indentation is necessary
+#else:
+ #   print('Its not the capital of Tamil Nadu')
+ 
+ #/////////////
+ 
+ #mark=int(input('Enter your mark: ')
+#if mark >= 90:
+#    print('GradeA')
+#elif mark>=80:
+#    print('GradeB')
+#elif mark>=70:
+#    print('GradeC')
+#elif mark>=60:
+#    print('GradeD')
+#elif mark>=50:
+#    print('GradeE')
+#else:
+#    print('Not Pass')
+
+#cap1='Chennai'
+#cap2='Bengaluru'
+#cap=input('Enter your capital: ')
+#if cap==cap1:
+#    print('State: Tamil Nadu')
+#elif cap==cap2:
+#    print('State: Karnataka')
+#else:
+#    print('Give some other capital')
+    
+#fuel=int(input('Enter ltr of petrol: '))
+#if fuel>=3:
+#    air=int(input('Enter air level(psi): '))
+#    if air>=30:
+#        print('Start journey')
+#    else:
+#       print('Check air')
+#else:
+#    print('Check fuel')
+
+name='pqrs'
+guess=input('Enter a name: ')
+while guess!=name:
+    guess=input('Incorrect name. Try another name: ')
+    if guess==name:
+        print('Correct')
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
