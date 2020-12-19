d6ef2f6cf9a6d1dfb49588add95dfc04112f108c
ilangovenac <acilangoven@gmail.com>
2020-12-19 09:21:27 +0530

    whileloop practice

---
+++ whileloop.py
@@ -0,0 +1,62 @@
+#n=1
+#while n<=5:
+ #   print(n, end=' ')
+#    n+=1   
+#text='abcdefg'
+#i=0
+#while i<len(text):
+#    print(text[i],end=' ')
+#    i+=1
+
+#n=1
+#while n<=50:
+# if n%5==0:
+#      print(n)
+#   n+=1
+
+#print('****')
+#n=1
+#count=0
+#while n<=50:
+#    if n%5==0:
+#        print(n, end=' ')
+#        count+=1
+#    n+=1
+#print('Count=',count)
+
+#text=input('Enter a text: ')
+#i=0
+#count=0
+#while i<len(text):
+#    if text[i] in ['a','e','i','o','u']:
+#        i+=1
+#        count+=1
+#        continue
+#    i+=1
+#print('No of vowels in text: ',count)
+
+i=1
+range=int(input('Enter upper range: '))
+while i <= range:
+    if i%6==0 and i%7==0:
+        print('First number divisible by 6 and 7 is: ',i)
+        break
+    i+=1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
