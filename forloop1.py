0069e8a800df884672ff08523104db5d9e4708a3
ilangovenac <acilangoven@gmail.com>
2020-12-23 21:13:22 +0530

    forloop intro & examples

---
+++ forloop1.py
@@ -0,0 +1,78 @@
+#name='Ilango'
+#for x in name:
+#    print(x,end=' ')
+#print()  
+#num=10
+#for x in range(5):
+#    print(x,end=' ')
+
+#for x in range(5,10):
+#     print(x,end=' ')
+
+#for x in range(5,15,2):
+#    print(x,end=' ')
+#print()
+#
+#for x in range(15,5,-1):
+#    print(x,end=' ')
+
+#for x in [1,5,6,3,4]:
+#    print(x,end=' ')
+
+
+#Program to check for a palindrome
+#text=input('Enter a text: ')
+#text2=''
+#length=len(text)
+#for x in range((length-1),-1,-1):
+#    text2+=text[x]
+#if text2==text:
+#   print('Palindrome')
+#else:
+#   print('Not Palindrome')
+
+#Program to find HCF of two numbers
+#lim=num1=int(input('Enter 1st number: '))
+#num2=int(input('Enter 2nd number: '))
+#if num1>num2:
+#    lim=num2
+#for x in range(lim,0,-1):
+#    if num1%x==0 and num2%x==0:
+#        print('HCF of two numbers:',x)
+#        break
+    
+#Program to reverse a number and check for palindrome (With input as number)
+#NOT YET COMPLETED*****XXXXXXXXX********
+#num=num2=int(input('Enter a number: '))
+#total=0
+#for num in range(0,):
+#    digit=num%10
+#    total=total*10 + digit
+#    num//=10
+#print('Reverse of number:',total)
+#if total==num2:
+#    print('Palindrome number')
+#else:
+#    print('Not Palindrome number')
+#*************XXXXXXXXXXXXX*************
+
+#Fibonacci series
+#n=int(input('Enter number of terms: '))
+#f,s=-1,1
+#for x in range(n):
+#    print(f+s,end=' ')
+#    s=f+s
+#    f=s-f
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
