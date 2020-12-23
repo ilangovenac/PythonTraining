751995201b9fe7b3b500ecdc1bd002acef6dcd3c
ilangovenac <acilangoven@gmail.com>
2020-12-23 06:49:29 +0530

    Whileloop4 practice

---
+++ whileloop4.py
@@ -0,0 +1,65 @@
+#Program to swap two variables without using a third variable
+#a=int(input('Enter value of a: '))
+#b=int(input('Enter value of b: '))
+
+#print(a,b)
+
+#b=a+b
+#a=b-a
+#b=b-a
+
+#print(a,b)
+
+#Program to print Fibonacci series without a third variable
+
+#first=-1
+#second=1
+#n=int(input('Enter range of Fibonacci series: '))
+#while n>0:
+#    print(first+second,end=' ')
+#    second=first+second
+#    first=second-first
+#    n-=1
+
+#Binary to Decimal
+#binary=int(input('Enter a binary number: '))
+#decimal=0
+#i=0
+#while binary>0:
+#    decimal+=(binary%10*(2**i))
+#    i+=1
+#    binary//=10
+#print('Decimal value:',decimal)
+
+#Importing and using modules
+#import math
+#n=int(input('Enter a number: '))
+#p=int(input('Enter power: '))
+#print(math.pow(n,p))
+#print(int(math.pow(n,p)))
+
+#import math
+#n=int(input('Enter a number: '))
+#print('Factoral of number:',math.factorial(n))
+#print('**********')
+#a=int(input('Enter first integer: '))
+#b=int(input('Enter second integer: '))
+#print('GCD of two numbers:',math.gcd(a,b))
+#print('LCM of two numbers:',math.lcm(a,b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
