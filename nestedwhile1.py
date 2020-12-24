209d4bfe441ff29ed633fda9fc8c3ab4fcd37750
ilangovenac <acilangoven@gmail.com>
2020-12-24 08:02:27 +0530

    nested while practice

---
+++ nestedwhile1.py
@@ -0,0 +1,76 @@
+#Nested while loop
+#row=1
+#while row<=5:#outerloop
+#    col=1
+#    while col<=5:#inner loop
+#        print(col,end=' ')
+#        col+=1
+#    print()#to line break 
+#    row+=1
+
+#row=1
+#while row<=3:#outerloop
+#    col=1
+#    while col<=4:#inner loop
+#        print(col+row,end=' ')
+#        col+=1
+#    print()#to line break 
+#    row+=1
+    
+#col<=row
+#row=1
+#while row<=4:#outerloop
+#    col=1
+#    while col<=row:#inner loop
+#        print(col,end=' ')
+#        col+=1
+#    print()#to line break 
+#    row+=1
+    
+#row+col
+#row=1
+#while row<=4:#outerloop
+#    col=1
+#    while col<=4:#inner loop
+#        print(row+col,end=' ')
+#        col+=1
+#    print()#to line break 
+#    row+=1
+    
+#col-row
+#row=1
+#while row<=4:#outerloop
+#    col=1
+#    while col<=4:#inner loop
+#        print(col-row,end=' ')
+#        col+=1
+#    print()#to line break 
+#    row+=1    
+
+#col*row
+row=1
+while row<=4:#outerloop
+    col=1
+    while col<=4:#inner loop
+        print(col*row,end=' ')
+        col+=1
+    print()#to line break 
+    row+=1  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
