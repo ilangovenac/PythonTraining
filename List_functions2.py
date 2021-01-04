9a8f6ec5924cd8436fa17a90a0282298b532e525
ilangovenac <acilangoven@gmail.com>
2021-01-04 21:33:11 +0530

    more list functions

---
+++ List_functions2.py
@@ -0,0 +1,47 @@
+#Sorting list
+lst=[23,12,45,35,11]
+lst1=['ravi','kumar','abc','xyz']
+lst.sort(reverse=True)
+lst1.sort(reverse=True)
+print(lst)
+print(lst1)
+
+#List aliasing and cloning
+lst1=[20,33,55]
+lst2=lst1 #List aliasing
+lst3=lst1[:] #List cloning
+print(id(lst1))
+print(id(lst2))
+print(id(lst3))
+
+#Operators
+lst1=[20,33,55]
+lst2=[10,33,55]
+lst3=['hi','hello']
+lst4=['abc','xyz']
+print(lst1!=lst2)
+print(lst2<lst1)
+print(lst1 is lst2)
+print(lst4<lst3)
+
+#Membership operator and clear() function
+lst=[34,23,54,'hi','the']
+print(34 in lst)
+print('old' in lst)
+print('hi' in lst)
+lst.clear()
+print(lst)
+
+#List comprehension
+lst1=[x for x in range(10)]
+print(lst1)
+
+lst2=[x for x in lst1 if x%2==0]
+print(lst2)
+
+lst=['hi','how','are','you']
+lst3=[lst[2] for x in lst]
+print(lst3)
+
+
+
